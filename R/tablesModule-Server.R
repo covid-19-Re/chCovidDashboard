aef3304b2a614037790696e2ad37f5084bb73791
Daniel Angst <dcangst@gmail.com>
2020-11-22 18:27:01 +0100

    add incidence

--- R/tablesModule-Server.R
+++ R/tablesModule-Server.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(lubridate)
 library(DT)
+library(slider)
 
 # calculate doubling time from Re using a Gamma distributed generation time 
 getDoublingTimeRe <- function(re, mu = 4.8, sigma = 2.3) {
@@ -10,7 +11,7 @@ getDoublingTimeRe <- function(re, mu = 4.8, sigma = 2.3) {
 
   beta <- (re^(1 / shape) - 1) * rate
 
-  # Epidemic doubling times 
+  # Epidemic doubling times
   d <- log(2) / beta
   d <- ifelse(d < 0, -d, d)
   return(d)
@@ -19,19 +20,19 @@ getDoublingTimeRe <- function(re, mu = 4.8, sigma = 2.3) {
 tablesServer <- function(id, trendsData) {
   moduleServer(
     id,
-    function(input, output, session) {
+    function (input, output, session) {
       observe({
         toExclude <- setdiff(names(input), "tab")
         setBookmarkExclude(toExclude)
       })
 
       rEstimatesPath <- reactive({
-        rEstimatesPaths <- "data/Re/CHE-estimates.csv"
+        rEstimatesPaths <- "data/Re/CHE-estimates.rds"
         # load newer file of public and regular app if in test app
         if (str_detect(getwd(), "testapp")) {
-          if (file.exists("../app/data/Re/CHE-estimates.csv")) {
-            rEstimatesPaths <- c("data/Re/CHE-estimates.csv",
-              "../app/data/Re/CHE-estimates.csv")
+          if (file.exists("../app/data/Re/CHE-estimates.rds")) {
+            rEstimatesPaths <- c("data/Re/CHE-estimates.rds",
+              "../app/data/Re/CHE-estimates.rds")
           }
         }
         rEstimatesPathmTime <- file.mtime(rEstimatesPaths)
@@ -43,14 +44,7 @@ tablesServer <- function(id, trendsData) {
 
       rEstimates <- reactive({
         rEstimatesPath <- rEstimatesPath()
-        rEstimates <- read_csv(rEstimatesPath$path,
-          col_types =
-            cols(
-              date = col_date(format = ""),
-              median_R_mean = col_double(),
-              median_R_highHPD = col_double(),
-              median_R_lowHPD = col_double(),
-              .default = col_character())) %>%
+        rEstimates <- readRDS(rEstimatesPath$path) %>%
           filter(
             !(str_detect(region, "grR")),
             data_type != "Confirmed cases / tests",
@@ -73,11 +67,46 @@ tablesServer <- function(id, trendsData) {
           mutate(across(.cols = tidyselect::starts_with("Re_"), getDoublingTimeRe, .names = "{.col}_dt"))
       })
 
+      incidenceData <- reactive({
+        rEstimatesPath <- rEstimatesPath()
+        incidencePath <- str_replace(rEstimatesPath$path, "estimates", "Data")
+
+        incidenceData <- readRDS(incidencePath) %>%
+          filter(
+            !(data_type %in% c("Stringency Index", "Confirmed cases / tests")),
+            date_type == "report_plotting", is.na(local_infection)) %>%
+          select(-local_infection, -date_type, -(positiveTests:country)) %>%
+          mutate(
+            value = replace_na(value, 0),
+            valueNorm = replace_na(value / populationSize * 100000, 0)) %>%
+          group_by(region, data_type) %>%
+          mutate(
+            value7day = slide_index_dbl(value, date, mean, .before = days(7)),
+            valueNorm7day = slide_index_dbl(valueNorm, date, mean, .before = days(7)),
+            value14day = slide_index_dbl(value, date, mean, .before = days(14)),
+            valueNorm14day = slide_index_dbl(valueNorm, date, mean, .before = days(14))
+          ) %>%
+          top_n(1, date) %>%
+          ungroup() %>%
+          transmute(
+            region = recode(region, "CHE" = "CH"),
+            age_class = "all",
+            event = recode(data_type,
+              "Confirmed cases" = "cases",
+              "Hospitalized patients" = "hospitalizations",
+              "Deaths" = "deaths"),
+            value7day = value7day,
+            valueNorm7day = valueNorm7day,
+            value14day = value14day,
+            valueNorm14day = valueNorm14day)
+      })
+
       comparisonData <- reactive({
         rEstimates <- rEstimates()
-        allData <- trendsData() %>%
+        incidenceData <- incidenceData()
+        allData <- incidenceData %>%
+          full_join(trendsData(), by = c("region", "age_class", "event")) %>%
           full_join(rEstimates, by = c("region", "age_class", "event"))
-
       })
 
       output$comparisonDataTable <- renderDataTable({
@@ -89,12 +118,15 @@ tablesServer <- function(id, trendsData) {
               th(rowspan = 2, "Region"),
               th(rowspan = 2, "Age class"),
               th(rowspan = 2, "Event"),
+              th(colspan = 2, "7 day mean incidence"),
+              th(colspan = 2, "14 day mean incidence"),
               th(colspan = 3, "Doubling time (d)"),
               th(colspan = 3, "Weekly change (%)"),
               th(colspan = 3, HTML("R<sub>e</sub><sup>1</sup>")),
               th(colspan = 3, HTML("<span style='color:red;'>Doubling time</span> / <span style='color:green;'>Half-life</span> (d) from R<sub>e</sub><sup>2</sup>"))
             ),
             tr(
+              lapply(rep(c("raw", "/100'000"), 2), th),
               lapply(rep(c("estimate", "lower 95% CI", "upper 95% CI"), 4), th),
             )
           ),
@@ -133,6 +165,12 @@ tablesServer <- function(id, trendsData) {
             dom = "t",
             pageLength = 125,
             lengthMenu = c(25, 50, 100, 125))) %>%
+          formatRound(
+            columns = c(
+              "value7day", "valueNorm7day",
+              "value14day", "valueNorm14day"
+            ),
+            digits = 2) %>%
           formatSignif(
             columns = c(
               "dt_estimate", "dt_lower", "dt_upper",
