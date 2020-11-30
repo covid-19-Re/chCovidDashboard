2e772ada14358db46a80df04032eac81a5fe2d72
Daniel Angst <dcangst@gmail.com>
2020-11-30 11:27:53 +0100

    update trends script

---
+++ R/trendsModule-Files/updateTrends.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+parent_path=$(
+  cd "$(dirname "${BASH_SOURCE[0]}")"
+  pwd -P
+)
+
+echo "$parent_path"
+
+runRScript () {
+  echo "running" $1 $2 "..."
+  Rscript --vanilla --verbose $1 $2 >>messages.Rout 2>>errors.Rout
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+    echo "Script didn't run successfully (Error" $retVal ")"
+  else
+    echo "       " $1 $2 "done"
+  fi
+}
+
+cd "$parent_path"
+rm *.Rout
+
+runRScript calculateTrends.R
